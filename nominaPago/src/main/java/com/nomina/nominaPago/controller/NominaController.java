ea8b54fa163c01a707b249096a220f1bb8e34bba
unknown <SSDD@PNAWSA05LC54512.duoc.acad>
2026-05-08 16:42:11 -0400

    Adicion de microservicio notificacion

--- nominaPago/src/main/java/com/nomina/nominaPago/controller/NominaController.java
+++ nominaPago/src/main/java/com/nomina/nominaPago/controller/NominaController.java
@@ -2,9 +2,17 @@ package com.nomina.nominaPago.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 @RestController
 @RequestMapping("/usuario/nomina")
 public class NominaController {
 
+    @GetMapping("/descarga")
+    public String getMethodName(@RequestParam String param) {
+        return new String();
+    }
+    
 }
---
+++ notificacion/src/test/java/com/noti/notificacion/NotificacionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.noti.notificacion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificacionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
