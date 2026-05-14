358543c92bfcc3c22901fd82011410d9edf69dd0
Manuel Ignacio Mora Acuña <man.moraa@duocuc.cl>
2026-05-13 22:36:44 -0400

    implemente errorDTO y manejos de errores para la aplicacion (solo la base)

---
+++ trabajadores/src/main/java/com/historial/trabajadores/dto/ErrorDTO.java
@@ -0,0 +1,28 @@
+package com.historial.trabajadores.dto;
+
+import java.time.LocalDateTime;
+import java.util.Map;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class ErrorDTO {
+
+    private LocalDateTime timestamp;
+    private int status;
+    private String mensaje;
+    private Map<String, String> errores;
+    private String path;
+
+    //contructor 
+    public ErrorDTO(LocalDateTime timestamp, int status, String mensaje, Map<String, String> errores, String path){
+        this.timestamp = timestamp;
+        this.status = status;
+        this.mensaje = mensaje;
+        this.errores = errores;
+        this.path = path;
+    }
+
+}
---
+++ trabajadores/src/main/java/com/historial/trabajadores/manejosErorres.java
@@ -0,0 +1,74 @@
+package com.historial.trabajadores;
+
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import com.historial.trabajadores.dto.ErrorDTO;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+@RestControllerAdvice // se encargara todos los errores del controlador
+public class manejosErorres {
+
+        //manejar errores de validacion.
+        @ExceptionHandler(MethodArgumentNotValidException.class)
+        public ResponseEntity<ErrorDTO> manejarValidaciones(
+            MethodArgumentNotValidException ex,
+            HttpServletRequest request){
+
+            Map<String, String> errores = new HashMap<>();
+            ex.getBindingResult().getFieldErrors().forEach(error -> errores.put(error.getField(), error.getDefaultMessage())); 
+            
+            ErrorDTO errorDTO = new ErrorDTO(
+                LocalDateTime.now(),
+                400,
+                "Errores de validaciones, intente de nuevo",
+                errores,
+                request.getRequestURI()
+            );
+            return ResponseEntity.badRequest().body(errorDTO);
+        }
+
+        // Manejar errores como datos duplicados etc.
+        @ExceptionHandler(DataIntegrityViolationException.class)
+        public ResponseEntity<ErrorDTO> manejarErrorBaseDatos(
+            DataIntegrityViolationException ex,
+            HttpServletRequest request) {
+
+                ErrorDTO errorDTO = new ErrorDTO(
+                    LocalDateTime.now(),
+                    400, // El codigo de error 
+                    "Error de integridad de datos, intente de nuevo",
+                    null,
+                    request.getRequestURI()
+                );
+                return ResponseEntity.badRequest().body(errorDTO);
+
+            }
+
+        // manejar errores como empleado no econtrado. etc
+        @ExceptionHandler(RuntimeException.class)
+        public ResponseEntity<ErrorDTO> manejarErrores(
+            RuntimeException ex,
+            HttpServletRequest request){
+
+                ErrorDTO errorDTO = new ErrorDTO(
+                    LocalDateTime.now(),
+                    404, // El codigo de error No encontrado
+                    ex.getMessage(), // el mensaje de error que lanzara
+                    null, // no hay errores de validacion, por eso es null
+                    request.getRequestURI());
+                    
+                    return ResponseEntity.badRequest().body(errorDTO);
+            }        
+}
+
+
+
