b21d3a0e1184ca8ce75da2e9b83ea44d9474ca53
Manuel Ignacio Mora Acuña <man.moraa@duocuc.cl>
2026-05-17 13:44:16 -0400

    Se termina microservicio historial empleados mas adelante se aplicara tokens para conectar auth

--- trabajadores/src/main/java/com/historial/trabajadores/controller/EmpleadoController.java
+++ trabajadores/src/main/java/com/historial/trabajadores/controller/EmpleadoController.java
@@ -2,9 +2,9 @@ package com.historial.trabajadores.controller;
 
 
 import java.util.List;
-import java.util.Optional;
 
-import org.springframework.beans.factory.annotation.Autowired;
+
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -21,11 +21,17 @@ import com.historial.trabajadores.service.EmpleadoService;
 
 import jakarta.validation.Valid;
 
+
+
 @RestController
 @RequestMapping("/empleados")
 public class EmpleadoController {
-    @Autowired
-    private EmpleadoService service;
+
+    private final EmpleadoService service;
+
+    public EmpleadoController(EmpleadoService service){
+        this.service = service;
+    }
 
     @GetMapping()
     public ResponseEntity<List<Empleado>> listarEmpleados() {
@@ -34,32 +40,22 @@ public class EmpleadoController {
 
     @GetMapping("/buscar/id/{id}")
     public ResponseEntity<?> buscarPorId(@PathVariable Integer id) {
-        Optional<EmpleadoDTO> empleado = service.buscarIdDTO(id);
-        try {
-            if (empleado.isEmpty()) {
-                return ResponseEntity.status(404).body("El id no existe");
-            }
-            return ResponseEntity.ok(empleado.get()); // mostrara los datos del empleado buscado (DTO)
-
-        } catch (Exception error) {
-            return ResponseEntity.status(500).body("error de la base de datos");
-
-        }
+        EmpleadoDTO empleadoBuscadoId = service.buscarIdDTO(id);
+        return ResponseEntity.ok().body(empleadoBuscadoId);
     }
 
     @GetMapping("/buscar/rut/{rut}")
     public ResponseEntity<?> buscarPorRut(@PathVariable String rut) {
-        Optional<EmpleadoDTO> empleado = service.buscarRutDTO(rut);
-        try {
-            if (empleado.isEmpty()) {
-                return ResponseEntity.status(404).body("EL rut que busca no existe");
-            }
-            return ResponseEntity.ok(empleado.get());
-        } catch (Exception error) {
-            return ResponseEntity.status(500).body("Error del sistema");
-
-        }
+       EmpleadoDTO empleadoBuscadoRut = service.buscarRutDTO(rut);
+       return ResponseEntity.ok().body(empleadoBuscadoRut);
     }
+
+    @GetMapping("/buscar/email/{email}")
+    public ResponseEntity<?> buscarPorEmail (@PathVariable String email){
+        EmpleadoDTO empleadoBuscadoEmail = service.buscarEmailDTO(email);
+        return ResponseEntity.ok().body(empleadoBuscadoEmail);
+    }
+    
     @PostMapping("/crear")
     public ResponseEntity<?> crearEmpleados(@Valid @RequestBody Empleado empleado) {
         Empleado empleadoNuevo = service.crearEmpleado(empleado);
--- trabajadores/src/main/java/com/historial/trabajadores/service/EmpleadoService.java
+++ trabajadores/src/main/java/com/historial/trabajadores/service/EmpleadoService.java
@@ -24,40 +24,7 @@ public class EmpleadoService {
         return repository.findAll();
     }
 
-    // Metodo get buscar empleado por id
-    public Optional<Empleado> buscarPorId(Integer id) {
-        if (id != null && id > 0) { // valido que el id sea mayor a 0
-            return repository.findById(id);
-        }
-        return Optional.empty();
-
-    }
-
-    // Metodo get buscar por rut
-    public Optional<Empleado> buscarPorRut(String rut) {
-        // Rut: Que no sea nulo y que no tenga espacios en blanco
-        if (rut == null || rut.isBlank()) {
-            return Optional.empty();
-        }
-        return repository.findByRut(rut);
-    }
-
-    // Metodo get buscar por email.
-    public Optional<Empleado> buscarPorEmail(String email) {
-        if (email == null || email.isBlank()) {
-            return Optional.empty();
-        }
-        return repository.findByEmailIgnoreCase(email);
-    }
-
-    // Metodo get buscar Por numeroTelefono
-    public Optional<Empleado> buscarPorNumeroTelefono(String numeroTelefono) {
-        if (numeroTelefono == null || numeroTelefono.isBlank()) {
-            return Optional.empty();
-        }
-        return repository.findByNumeroTelefono(numeroTelefono);
-    }
-
+    // metodo reatilizable para validar el empleado, se usa en crear y actualizar
     public void validarEmpleado(Empleado empleadoNuevo){
         if(empleadoNuevo == null){
             throw new IllegalArgumentException("El empleado no puede ser nulo");
@@ -87,11 +54,13 @@ public class EmpleadoService {
         if (id == null) {
             throw new IllegalArgumentException("El id no puede estar null");
         }
-        validarEmpleado(empleadoNuevo); // validamos el empleado del metodo reautilzado
+
         Optional<Empleado> existe = repository.findById(id);
         if (existe.isEmpty()) {
             throw new RuntimeException("El id que desea actualizar no existe");
         }
+
+        validarEmpleado(empleadoNuevo); // validamos el empleado del metodo reautilzado
         
         Empleado empleado = existe.get();
         empleado.setRut(empleadoNuevo.getRut());
@@ -133,38 +102,42 @@ public class EmpleadoService {
     }
 
     // Metodo DTO buscar por id
-    public Optional<EmpleadoDTO> buscarIdDTO(Integer id) {
-        Optional<Empleado> empleados = repository.findById(id);
-        if (empleados.isEmpty()) {
-            return Optional.empty();
+    //muy util porque el id es unico de cada empleado
+    public EmpleadoDTO buscarIdDTO(Integer id) {
+        if (id == null || id <= 0){
+            throw new IllegalArgumentException("El id no puede ser nulo ni menor o igual a 0");
         }
-        return Optional.of(empleadoDto(empleados.get()));
+        // buscamos el id y validamos que exista para luego hacerlo en DTO
+        Optional<Empleado> empleadoBuscado = repository.findById(id);
+        if (empleadoBuscado.isEmpty()) {
+            throw new RuntimeException("El id que desea buscar no existe");  
+        }
+        return empleadoDto(empleadoBuscado.get());
     }
 
     // Metodo DTO buscar por rut
-    public Optional<EmpleadoDTO> buscarRutDTO(String rut) {
-        Optional<Empleado> empleados = repository.findByRut(rut);
-        if (empleados.isEmpty()) {
-            return Optional.empty();
+    //buscar rut hace similitud con id porque rut es unico de cada empleado.
+    public EmpleadoDTO buscarRutDTO(String rut) {
+        if(rut == null || rut.isBlank()){
+            throw new IllegalArgumentException("El rut del empleado no puede estar en nulo ni en blanco");
         }
-        return Optional.of(empleadoDto(empleados.get()));
+        Optional<Empleado> empleadoBuscado = repository.findByRut(rut);
+        if (empleadoBuscado.isEmpty()) {
+            throw new RuntimeException("El rut que desea buscar no existe");
+        }
+        return empleadoDto(empleadoBuscado.get());
     }
 
     // metodo DTO buscar por email
-    public Optional<EmpleadoDTO> buscarEmailDTO(String email) {
-        Optional<Empleado> empleados = repository.findByEmailIgnoreCase(email);
-        if (empleados.isEmpty()) {
-            return Optional.empty();
+    //el email es unico y mas adelante cuando tengamos tokens lo conectamos con el login
+    public EmpleadoDTO buscarEmailDTO(String email) {
+        if(email == null || email.isBlank()){
+            throw new IllegalArgumentException("El email del empleado no puede estar en nulo ni en blanco");
         }
-        return Optional.of(empleadoDto(empleados.get()));
-    }
-
-    // metodo DTO buscar por numero telefono
-    public Optional<EmpleadoDTO> buscarNumeroTelefonoDTO(String numeroTelefono) {
-        Optional<Empleado> empleados = repository.findByNumeroTelefono(numeroTelefono);
-        if (empleados.isEmpty()) {
-            return Optional.empty();
+        Optional<Empleado> empleadoBuscado = repository.findByEmailIgnoreCase(email);
+        if (empleadoBuscado.isEmpty()) {
+            throw new RuntimeException("El email que desea buscar no existe");
         }
-        return Optional.of(empleadoDto(empleados.get()));
+        return empleadoDto(empleadoBuscado.get());
     }
 }
