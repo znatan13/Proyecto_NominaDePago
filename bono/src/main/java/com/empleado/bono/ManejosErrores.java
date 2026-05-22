b693334962fdabf129d4f83897b4e2e124646664
Manuel Ignacio Mora Acuña <man.moraa@duocuc.cl>
2026-05-22 15:35:46 -0400

    se termina el microservicio bonos y se inician pruebas en postman para probar su funcionalidad

---
+++ bono/src/main/java/com/empleado/bono/ManejosErrores.java
@@ -0,0 +1,80 @@
+package com.empleado.bono;
+
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import com.empleado.bono.dto.ErrorDTO;
+import jakarta.servlet.http.HttpServletRequest;
+
+@RestControllerAdvice
+public class ManejosErrores {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorDTO> manejosErroresGlobal (
+        MethodArgumentNotValidException ex,
+        HttpServletRequest request ){
+
+            Map<String, String> error = new HashMap<>();
+            ex.getBindingResult().getFieldErrors().forEach(errores -> error.put(errores.getField()
+            , errores.getDefaultMessage()));
+
+            ErrorDTO errorDTO = new ErrorDTO(
+                LocalDateTime.now(),
+                400,
+                "Error de validaciones, intente de nuevo",
+                error,
+                request.getRequestURI()
+            );
+            return ResponseEntity.badRequest().body(errorDTO);
+        }
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    public ResponseEntity<ErrorDTO> manejosErroresBd(
+        DataIntegrityViolationException ex,
+        HttpServletRequest request){
+
+            ErrorDTO errorDTO = new ErrorDTO(
+                LocalDateTime.now(),
+                400,
+                "Error de integridad de datos, intente de nuevo",
+                null,
+                request.getRequestURI()
+            );
+            return ResponseEntity.badRequest().body(errorDTO);
+        }
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<ErrorDTO> manejarExistenciaDatos(
+        RuntimeException ex,
+        HttpServletRequest request){
+
+        ErrorDTO errorDTO = new ErrorDTO(
+            LocalDateTime.now(),
+            404,
+            ex.getMessage(),
+            null,
+            request.getRequestURI()
+        );
+        return ResponseEntity.status(404).body(errorDTO);
+    }
+    
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<ErrorDTO> manejarDatosCorruptos(
+        IllegalArgumentException ex,
+        HttpServletRequest request){
+
+            ErrorDTO errorDTO = new ErrorDTO(
+                LocalDateTime.now(),
+                400,
+                ex.getMessage(),
+                null,
+                request.getRequestURI()
+            );
+            return ResponseEntity.status(400).body(errorDTO);
+        }
+}
---
+++ bono/src/main/java/com/empleado/bono/controller/BonoController.java
@@ -0,0 +1,77 @@
+package com.empleado.bono.controller;
+
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.empleado.bono.dto.BonosEmpleado;
+import com.empleado.bono.model.Bono;
+import com.empleado.bono.service.bonoService;
+
+import jakarta.validation.Valid;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@RestController
+@RequestMapping("/bonos")
+public class BonoController {
+
+    private bonoService service;
+
+    public BonoController(bonoService service){
+        this.service = service;
+    }
+    @GetMapping()
+    //metodo listar los bonos disponibles
+    public ResponseEntity<List<Bono>> listar(){
+        return ResponseEntity.ok().body(service.listarBonos());
+    }
+    //metodo crear un bono
+    @PostMapping("/crear")
+    public ResponseEntity<?> crearBono (@Valid @RequestBody Bono bonoNuevo) {
+        Bono bonoCreado = service.crearBono(bonoNuevo);
+        return ResponseEntity.status(201).body(bonoCreado);
+    }
+    //metodo actualizar un Bono por su id
+    @PutMapping("/actualizar/{bonoId}")
+    public ResponseEntity<?> actualizarBono(@PathVariable Integer bonoId, @RequestBody Bono bonoNuevo){
+        Bono bonoActualizar = service.actualizarBono(bonoId, bonoNuevo);
+        return ResponseEntity.ok().body(bonoActualizar);
+    }
+    //metodo buscar bono por su id
+    @GetMapping("/buscar/{bonoId}")
+    public ResponseEntity<?> buscarBono (@PathVariable Integer bonoId) {
+        Bono buscar = service.buscarBono(bonoId);
+        return ResponseEntity.ok().body(buscar);
+    }
+    //metodo buscar un empleado por id y mostrar bono (solo id)
+    @GetMapping("/buscar/{empleadoId}")    
+    public ResponseEntity<?> buscarEmpleadoId(@PathVariable Integer bonoId){
+        List<Bono> buscar = service.buscarIdEmpleado(bonoId);
+        return ResponseEntity.ok().body(buscar);
+    }
+    //metodo buscar los bonos con empleados asociados a tal bono
+    @GetMapping("/bonos/empleados/{bonoId}")
+    public ResponseEntity<?> bonosEmpleado (@PathVariable Integer bonoId) {
+        BonosEmpleado bonosEmpleado = service.bonosEmpleado(bonoId);
+        return ResponseEntity.ok().body(bonosEmpleado);
+    }
+    @DeleteMapping("/eliminar/{bonoId}")
+    public ResponseEntity<?> eliminarBono (@PathVariable Integer bonoId){
+        service.eliminarBono(bonoId);
+        return ResponseEntity.ok().body("El bono ha sido eliminado");
+    }
+    
+
+    
+    
+    
+
+}
