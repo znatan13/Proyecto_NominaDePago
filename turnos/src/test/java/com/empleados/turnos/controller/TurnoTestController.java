ca90ecdf4377b32983242e9b7e929925c7153142
Manuel Ignacio Mora Acuña <man.moraa@duocuc.cl>
2026-06-20 15:41:50 -0400

    Se actualiza pom.xml de microservicios de turno y se registra en eureka

--- turnos/src/test/java/com/empleados/turnos/controller/TurnoTestController.java
+++ turnos/src/test/java/com/empleados/turnos/controller/TurnoTestController.java
@@ -15,8 +15,8 @@ import java.util.List;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 import com.empleados.turnos.dto.EmpleadoSimple;
@@ -30,7 +30,7 @@ public class TurnoTestController {
     @Autowired
     private MockMvc mockMvc;
 
-    @MockBean
+    @MockitoBean
     private TurnosService service;
 
     @Test
