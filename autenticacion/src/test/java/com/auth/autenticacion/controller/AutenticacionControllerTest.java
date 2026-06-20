ec03e99afc629fcbf9e324f7b7a9bc772469ddf0
Manuel Ignacio Mora Acuña <man.moraa@duocuc.cl>
2026-06-20 14:51:33 -0400

    Se actualiza la version de pom.xlm y se registra autenticacion en eureka

--- autenticacion/src/test/java/com/auth/autenticacion/controller/AutenticacionControllerTest.java
+++ autenticacion/src/test/java/com/auth/autenticacion/controller/AutenticacionControllerTest.java
@@ -4,9 +4,9 @@ import java.util.List;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
 import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.mockito.ArgumentMatchers.any;
@@ -34,7 +34,7 @@ public class AutenticacionControllerTest {
     @Autowired
     public MockMvc mockMvc;
     
-    @MockBean
+    @MockitoBean
     private UsuarioService service;
 
     @Test
