fdd4f986688d80167f54513ccf1ac39da9b2573a
Manuel Ignacio Mora Acuña <man.moraa@duocuc.cl>
2026-06-20 15:12:21 -0400

    Se registra microservicio empleados a eureka y se actualiza pom.xml

--- trabajadores/src/test/java/com/historial/trabajadores/controller/EmpleadoControllerTest.java
+++ trabajadores/src/test/java/com/historial/trabajadores/controller/EmpleadoControllerTest.java
@@ -14,8 +14,8 @@ import java.util.List;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 
@@ -28,7 +28,7 @@ public class EmpleadoControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
-    @MockBean
+    @MockitoBean
     private EmpleadoService service;
 
     @Test
