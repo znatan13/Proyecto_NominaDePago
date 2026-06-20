4ef424b1924a78737edac05768f1cf5fac81b9de
Manuel Ignacio Mora Acuña <man.moraa@duocuc.cl>
2026-06-20 16:17:06 -0400

    Se actualiza pom.xml y se registra microservicio Bonos en eureka

--- bono/src/test/java/com/empleado/bono/controller/BonoControllerTest.java
+++ bono/src/test/java/com/empleado/bono/controller/BonoControllerTest.java
@@ -15,8 +15,8 @@ import java.util.List;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 import com.empleado.bono.dto.BonosEmpleado;
@@ -29,7 +29,7 @@ public class BonoControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
-    @MockBean
+    @MockitoBean
     private bonoService service;
 
     @Test
