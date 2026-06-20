4bdc41ee9e393d5115804af5dff92684645321b3
Manuel Ignacio Mora Acuña <man.moraa@duocuc.cl>
2026-06-20 15:58:23 -0400

    Se actualiza pom.xml de microservicio asistencia y se registra en eureka

--- asistencia/src/test/java/com/control/asistencia/controller/AsistenciaTestController.java
+++ asistencia/src/test/java/com/control/asistencia/controller/AsistenciaTestController.java
@@ -15,8 +15,8 @@ import java.util.List;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 import com.control.asistencia.dto.AsistenciaCompletaDTO;
@@ -29,7 +29,7 @@ public class AsistenciaTestController {
     @Autowired
     private MockMvc mockMvc;
 
-    @MockBean
+    @MockitoBean
     private AsistenciaService service;
 
     @Test
