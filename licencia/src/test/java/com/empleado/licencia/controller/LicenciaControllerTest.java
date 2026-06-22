0da2c3efefea241a2c6a87a0733be87833262558
Manuel Ignacio Mora Acuña <man.moraa@duocuc.cl>
2026-06-21 22:34:00 -0400

    Se implementa la base de test para que bastian lo desarrolle

--- licencia/src/test/java/com/empleado/licencia/controller/LicenciaControllerTest.java
+++ licencia/src/test/java/com/empleado/licencia/controller/LicenciaControllerTest.java
@@ -1,3 +1,19 @@
+package com.empleado.licencia.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import com.empleado.licencia.service.LicenciaService;
+
+@WebMvcTest(LicenciaController.class)
 public class LicenciaControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockitoBean
+    private LicenciaService service;
 
 }
