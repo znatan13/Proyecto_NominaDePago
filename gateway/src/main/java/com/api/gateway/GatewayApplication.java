ccc224153868d2f0c2caf1f966a1e0559322f8a9
Manuel Ignacio Mora Acuña <man.moraa@duocuc.cl>
2026-06-22 21:18:54 -0400

    Se añade gateway y se configura con los microservicios del proyecto

---
+++ gateway/src/main/java/com/api/gateway/GatewayApplication.java
@@ -0,0 +1,19 @@
+package com.api.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class GatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayApplication.class, args);
+		System.out.println("=====================================");
+		System.out.println("Api Gateway corriendo exitosamente");
+		System.out.println("URL: http://localhost:8090");
+		System.out.println("=====================================");
+	}
+
+}
